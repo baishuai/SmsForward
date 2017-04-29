3e6e11526a509575ad5052cbe449bd8eee2b121b
Bai Shuai <baishuai.io@foxmail.com>
2017-04-29 21:44:01 +0800

    catch exception when using http Post

--- app/src/main/java/baishuai/github/io/smsforward/forward/FeigeRepo.kt
+++ app/src/main/java/baishuai/github/io/smsforward/forward/FeigeRepo.kt
@@ -2,6 +2,7 @@ package baishuai.github.io.smsforward.forward
 
 import android.telephony.SmsMessage
 import android.util.Log
+import java.io.IOException
 import javax.inject.Inject
 
 /**
@@ -12,9 +13,14 @@ class FeigeRepo @Inject constructor(private val api: FeigeApi) {
     fun forward(sms: SmsMessage) {
         val call = api.forward("status", sms.displayMessageBody.substring(0, minOf(6, sms.displayMessageBody.length)),
                 sms.originatingAddress, sms.messageBody)
-        val response = call.execute()
-        if (response.isSuccessful) {
-            Log.d(this.javaClass.canonicalName, response.body().code.toString() + response.body().msg)
+        try {
+            val response = call.execute()
+            if (response.isSuccessful) {
+                Log.d(this.javaClass.canonicalName, response.body().code.toString() + response.body().msg)
+            }
+        } catch (e: IOException) {
+            //TODO: forward sms by sending sms directly
+            Log.d(this.javaClass.canonicalName, e.toString())
         }
     }
 }
